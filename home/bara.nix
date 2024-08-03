703ff827b5b6673e017aeff427409101ddf49bcc
brauni@bara <braunnicolaj@gmail.com>
2024-08-03 12:19:32 +0200

    firefox

---
+++ home/bara.nix
@@ -0,0 +1,34 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    ./apps/firefox.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "brauni@bara";
+    userEmail = "braunnicolaj@gmail.com";
+  };
+}
