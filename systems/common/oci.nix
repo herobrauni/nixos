63b80d172f3782b2617fe2c81bf5e18c269cdd4b
brauni@envy <braunnicolaj@gmail.com>
2024-09-01 21:46:46 +0200

    update

--- systems/common/oci.nix
+++ systems/common/oci.nix
@@ -61,6 +61,15 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmpZL3J2RqRK7ynIgowaZBKzI+EiuCGmwB6l0AxLk1v"
   ];
   users.users.root.initialHashedPassword = "$y$j9T$d7EVWIrLInhGgEObbWa0A1$jomM5R056rhtJOOBH5vxC6GRnPMdqCb23ZKNWvqv1L9";
+  users.users.brauni.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPFkI1tmXLQ5awKEqqoEUMbCalSqARtODdy8nQ18pKk"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETmaz2oKUkpoSSeGKQefhFb+PUCEwY9Onh9+q1+hXXt"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICvklcYlHcJVJzEAmkevC9eZ/rjCN7d1jhDHMBbVSmkqAAAABHNzaDo="
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJUzimGYl+VtbaQVuGkVRwxRBMQEJDsmD5g+YeHx2s9bAAAABHNzaDo="
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINz7Y1oRX+SURSXOoNv5se/hrpi6VvLHK0T3zqz+q5kqAAAABHNzaDo="
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqCCBIJ2ntASqsNfAt0aKXf7usA1kmJCHKOKldQk9Tx"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmpZL3J2RqRK7ynIgowaZBKzI+EiuCGmwB6l0AxLk1v"
+  ];
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
