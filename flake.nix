aa42c8ec9b54bc955e2b810e5cee89d034edbbcd
herobrauni <braunnicolaj@gmail.com>
2024-10-19 16:13:29 +0200

    update

--- flake.nix
+++ flake.nix
@@ -28,111 +28,108 @@
     };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , home-manager
-    , systems
-    , lix-module
-    , ...
-    } @ inputs:
-    let
-      inherit (self) outputs;
-      lib = nixpkgs.lib // home-manager.lib;
-      forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
-      pkgsFor = lib.genAttrs (import systems) (
-        system:
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    systems,
+    lix-module,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs (import systems) (
+      system:
         import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         }
-      );
-    in
-    {
-      inherit lib;
+    );
+  in {
+    inherit lib;
 
-      # devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
-      formatter = forEachSystem (pkgs: pkgs.alejandra);
+    # devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
 
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
-        # envy
-        envy = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./systems/envy
-            lix-module.nixosModules.default
-          ];
-          specialArgs = {
-            inherit inputs outputs;
-          };
-        };
-        # desk
-        desk = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./systems/desk
-            lix-module.nixosModules.default
-          ];
-          specialArgs = {
-            inherit inputs outputs;
-          };
-        };
-        # k3s-oci-arm
-        k3s-oci-arm-2 = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./systems/k3s-oci-arm-2
-          ];
-          specialArgs = {
-            inherit inputs outputs;
-          };
-        };
-        k3s-oci-arm-3 = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./systems/k3s-oci-arm-3
-          ];
-          specialArgs = {
-            inherit inputs outputs;
-          };
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # envy
+      envy = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./systems/envy
+          lix-module.nixosModules.default
+        ];
+        specialArgs = {
+          inherit inputs outputs;
         };
       };
-
-
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        # envy laptop
-        "brauni@envy" = lib.homeManagerConfiguration {
-          modules = [
-            ./home/envy.nix
-            ./home/home.nix
-          ];
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
+      # desk
+      desk = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./systems/desk
+          lix-module.nixosModules.default
+        ];
+        specialArgs = {
+          inherit inputs outputs;
         };
-        # desk
-        "brauni@bazzite" = lib.homeManagerConfiguration {
-          modules = [
-            ./home/desk.nix
-            ./home/home.nix
-          ];
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
+      };
+      # k3s-oci-arm
+      k3s-oci-arm-2 = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./systems/k3s-oci-arm-2
+        ];
+        specialArgs = {
+          inherit inputs outputs;
         };
-        # cachy
-        "brauni@cachy" = lib.homeManagerConfiguration {
-          modules = [
-            ./home/cachy.nix
-            ./home/home.nix
-          ];
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
+      };
+      oci-small-1 = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./systems/oci-small-1
+        ];
+        specialArgs = {
+          inherit inputs outputs;
         };
       };
     };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # envy laptop
+      "brauni@envy" = lib.homeManagerConfiguration {
+        modules = [
+          ./home/envy.nix
+          ./home/home.nix
+        ];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {
+          inherit inputs outputs;
+        };
+      };
+      # desk
+      "brauni@bazzite" = lib.homeManagerConfiguration {
+        modules = [
+          ./home/desk.nix
+          ./home/home.nix
+        ];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {
+          inherit inputs outputs;
+        };
+      };
+      # cachy
+      "brauni@cachy" = lib.homeManagerConfiguration {
+        modules = [
+          ./home/cachy.nix
+          ./home/home.nix
+        ];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {
+          inherit inputs outputs;
+        };
+      };
+    };
+  };
 }
